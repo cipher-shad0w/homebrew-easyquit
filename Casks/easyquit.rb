20d8c3dca81e022cf63cb1673fb2b9ae9cd95678
Jannis Krija <107557730+cipher-shad0w@users.noreply.github.com>
2025-11-13 12:03:24 +0100

    Update EasyQuit to version 1.1.0

--- Casks/easyquit.rb
+++ Casks/easyquit.rb
@@ -1,6 +1,6 @@
 cask "easyquit" do
-  version "1.0.0"
-  sha256 "544afc4182497b9af32f613e8dbd1742e0a6a751bfd89dae64293058621ec951"
+  version "1.1.0"
+  sha256 "3248a57976e4075926d5bbe7e33d5fc25ed933b7a63e8fd9ec7057ba2db27dab"
 
   url "https://github.com/cipher-shad0w/EasyQuit/releases/download/v#{version}/EasyQuit.zip"
   name "EasyQuit"
